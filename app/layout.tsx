f6fcb11021d0dd5a1bbb12f5001f26f2f7602423
harumiWeb <ganaharumi@outlook.jp>
2025-01-06 23:43:37 +0900

    Update layout and enhance Tauri capabilities
    - Added metadata for the application in `layout.tsx`, including title and description.
    - Expanded Tauri capabilities by including global shortcut permissions in `default.json` and `desktop.json`, improving keyboard shortcut functionality.
    
    These changes aim to enhance the application's metadata and improve user interaction through keyboard shortcuts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import WindowMenu from "./components/WindowMenu/WindowMenu";
-
+import type { Metadata } from "next";
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Tavif",
+  description: "Tavif is a tool to convert images to avif format.",
+};
 
 export default function RootLayout({
   children,
